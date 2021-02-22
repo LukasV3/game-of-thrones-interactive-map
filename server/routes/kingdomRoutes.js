0d8cc3a00b5df0053ef7b673678140821f158b7f
LukasV3 <volk.lukas237@yahoo.co.uk>
2021-02-22 17:10:53 +0000

    updated gitignore

--- server/routes/kingdomRoutes.js
+++ server/routes/kingdomRoutes.js
@@ -3,11 +3,6 @@ const express = require("express");
 
 const router = express.Router();
 
-// Hello World Test Endpoint
-router.get("/hello", (req, res, next) => {
-  res.send("Hello World");
-});
-
 router.get("/", kingdomController.getKingdomBoundaries);
 router.get("/:id/summary", kingdomController.getSummary);
 
